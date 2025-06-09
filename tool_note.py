b098f4b22c319af7c97fd4f6c097a30710508d6c
Valentin <valoliinykov.work@gmail.com>
2025-06-09 15:06:23 +0300

    Update tool_note.py
    add statuys

--- tool_note.py
+++ tool_note.py
@@ -7,13 +7,14 @@ class Note:
     def __init__(self, title: str, content: str):
         self.id = str(uuid.uuid4())
         self.title = title
-        # self.content = content
-        # self.created_at = datetime.now().isoformat()
+        self.content = content
+        self.created_at = datetime.now().isoformat()
+        self.status = True
 
     def to_dict(self):
         return {
             "id": self.id,
             "title": self.title,
-            # "content": self.content,
-            # "created_at": self.created_at
+            "content": self.content,
+            "created_at": self.created_at
         }
