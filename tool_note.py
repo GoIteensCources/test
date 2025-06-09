739afc2df85154bbaca44813f168bf88e8363f24
Walentin <valoliinykov.work@gmail.com>
2025-06-09 15:09:41 +0300

    tool

--- tool_note.py
+++ tool_note.py
@@ -6,14 +6,16 @@ import uuid
 class Note:
     def __init__(self, title: str, content: str):
         self.id = str(uuid.uuid4())
+        self.status = False
         self.title = title
-        # self.content = content
-        # self.created_at = datetime.now().isoformat()
+        self.content = content
+        self.created_at = datetime.now().isoformat()
 
     def to_dict(self):
-        return {
-            "id": self.id,
-            "title": self.title,
-            # "content": self.content,
-            # "created_at": self.created_at
-        }
+        if self.status:
+            return {
+                "id": self.id,
+                "title": self.title,
+                "content": self.content,
+                "created_at": self.created_at
+            }
